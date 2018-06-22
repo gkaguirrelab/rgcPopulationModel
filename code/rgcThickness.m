e1d461e36a9f6145caa0d233effebeecaff060a0
gkaguirre <gkaguirre@me.com>
2018-06-21 23:46:24 -0400

    added some details
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/rgcThickness.m
+++ code/rgcThickness.m
@@ -51,87 +51,94 @@ end
 %   cells in human retina." Journal of comparative Neurology 300.1 (1990):
 %   5-25.
 
-% Data from Curcio & Allen 1990, Figure 10
-amacrine.density.supportMM.temporal = [0, 0.11, 0.23, 0.4, 0.65, 0.86, 1.45, 2.46, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.6, 12.6, 13.6, 14.6, 15.6, 16.6, 17.7, 18.65, 19.65];
-amacrine.density.countsMMSq.temporal = [73, 124, 317, 542, 689, 813, 1052, 1112, 1159, 1187, 1146, 1069, 992, 942, 888, 848, 798, 767, 731, 699, 677, 650, 642, 652, 676];
-
-% Obtain a spline fit to the amacrine densities
-for mm = 3:3 %length(p.Results.cardinalMeridianAngles)
+for mm = 1:length(p.Results.cardinalMeridianAngles)
+    % Data from Curcio & Allen 1990, Figure 10, used for all meridians
+    amacrine.density.supportMM.(p.Results.cardinalMeridianNames{mm}) = [0, 0.11, 0.23, 0.4, 0.65, 0.86, 1.45, 2.46, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.6, 12.6, 13.6, 14.6, 15.6, 16.6, 17.7, 18.65, 19.65];
+    amacrine.density.countsMMSq.(p.Results.cardinalMeridianNames{mm}) = [73, 124, 317, 542, 689, 813, 1052, 1112, 1159, 1187, 1146, 1069, 992, 942, 888, 848, 798, 767, 731, 699, 677, 650, 642, 652, 676];
+    % Obtain a spline fit to the amacrine densities
     amacrine.density.fitMMSq.(p.Results.cardinalMeridianNames{mm}) = ...
         fit(amacrine.density.supportMM.(p.Results.cardinalMeridianNames{mm})', amacrine.density.countsMMSq.(p.Results.cardinalMeridianNames{mm})', 'smoothingspline');
 end
 
-
-% Amacrine cell body sizes
-% Data from Curcio & Allen 1990, Figure 3
-% The support for this is the frequency it is measured in, I assumed percentages but it adds up to well over 100%, ask about x & y axes
+% Amacrine cell body sizes Data from Curcio & Allen 1990, Figure 3 The
+% support for this is the frequency it is measured in, I assumed
+% percentages but it adds up to well over 100%, ask about x & y axes
 amacrine.diameter.supportMM = [1, 3.95, 11.95, 55, 34.1, 11, 13, 6, 4.95, 0.8, 0.9];
 amacrine.diameter.sizeMM = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+amacrine.diameter.fitMM = @(x) 0.01;
 
 %% Parasol RGCs
-% Average parasol cell densities across four meridians measured in cells/square mm from six macaque retinas
+% Average parasol cell densities across four meridians measured in
+% cells/square mm from six macaque retinas
 % 
-% Silveira, L. C. L., and V. H. Perry. "The topography of magnocellular projecting ganglion cells (M-ganglion cells)
-% in the primate retina." Neuroscience 40.1 (1991): 217-237.
+%   Silveira, L. C. L., and V. H. Perry. "The topography of magnocellular
+%   projecting ganglion cells (M-ganglion cells) in the primate retina."
+%   Neuroscience 40.1 (1991): 217-237.
 
-% Data from Silviera et al. 1991, Figures 16
-parasol.density.supportMM.temporal = 1:1:22;
-parasol.density.countsMMSq.temporal = [2150, 1550, 900, 460, 300, 220, 170, 110, 70, 56, 40, 20, 10, nan, nan, nan, nan, nan, nan, nan, nan, nan];
-parasol.density.supportMM.nasal = 1:1:22;
-parasol.density.countsMMSq.nasal = [3350, 2560, nan, nan, nan, 520, 485, 415, 400, 380, 345, 280, 255, 250, 230, 195, 150, 130, 65, 35, 30, 20];
-parasol.density.supportMM.superior = 1:1:22;
-parasol.density.countsMMSq.superior = [1610, 700, 405, 300, 240, 175, 140, 95, 80, 70, 55, 50, 35, 25, 30, 30, 40, nan, nan, nan, nan, nan];
-parasol.density.supportMM.inferior = 1:1:22;
-parasol.density.countsMMSq.inferior = [1820, 1100, 725, 370, 270, 205, 170, 110, 95, 75, 53, 48, 25, 05, nan, nan, nan, nan, nan, nan, nan];
-
-% Obtain a spline fit to the parasol densities
-for mm = 1:length(p.Results.cardinalMeridianAngles)
-    nonNanSupportIdx = ~isnan(parasol.density.countsMMSq.(p.Results.cardinalMeridianNames{mm}));
-    parasol.density.fitMMSq.(p.Results.cardinalMeridianNames{mm}) = ...
-        fit(parasol.density.supportMM.(p.Results.cardinalMeridianNames{mm})(nonNanSupportIdx)', parasol.density.countsMMSq.(p.Results.cardinalMeridianNames{mm})(nonNanSupportIdx)', 'smoothingspline');
-end
+% % Data from Silviera et al. 1991, Figures 16
+% parasol.density.supportMM.temporal = 1:1:22;
+% parasol.density.countsMMSq.temporal = [2150, 1550, 900, 460, 300, 220, 170, 110, 70, 56, 40, 20, 10, nan, nan, nan, nan, nan, nan, nan, nan, nan];
+% parasol.density.supportMM.nasal = 1:1:22;
+% parasol.density.countsMMSq.nasal = [3350, 2560, nan, nan, nan, 520, 485, 415, 400, 380, 345, 280, 255, 250, 230, 195, 150, 130, 65, 35, 30, 20];
+% parasol.density.supportMM.superior = 1:1:22;
+% parasol.density.countsMMSq.superior = [1610, 700, 405, 300, 240, 175, 140, 95, 80, 70, 55, 50, 35, 25, 30, 30, 40, nan, nan, nan, nan, nan];
+% parasol.density.supportMM.inferior = 1:1:22;
+% parasol.density.countsMMSq.inferior = [1820, 1100, 725, 370, 270, 205, 170, 110, 95, 75, 53, 48, 25, 05, nan, nan, nan, nan, nan, nan, nan];
+% 
+% 
 
 
 % Parasol proportion of all retinal ganglion cells
 % Data from Silviera et al. 1991, Figure 17
-parasol.proportion.supportMM.nasal = [1.06, 2.07, 3.12, 4.12, 5.09, 6.17, 7.19, 8.22, 9.2, 10.24, 11.26, 12.24, 13.31, 14.32, 15.32, 16.42, 17.39];
-parasol.proportion.countsMMSq.nasal = [9.8, 9.64, nan, nan, nan, 7.58, 9.5, 11.74, 13.98, 19.18, 20.78, 15.99, 19.83, 20.32, 24.55, 26.87, 19.93];
-parasol.proportion.supportMM.temporal = [1.06, 2.07, 3.12, 4.12, 5.09, 6.17, 7.19, 8.22, 9.2, 10.24, 11.26, 12.24, 13.31, 14.32, 15.32, 16.42, 17.39];
-parasol.proportion.countsMMSq.temporal = [6.6, 8.44, 9.09, 8.45, 8.85, 10.54, 10.86, 9.51, 9.27, 8.56, 5.69, 2.97, 4.98, nan, nan, nan, nan];
-parasol.proportion.supportMM.superior = [.96, 1.99, 2.98, 3.93, 4.93, 5.99, 6.87, 7.94, 8.89, 9.88, 10.92, 11.91, 12.9, 13.89, 14.92];
-parasol.proportion.countsMMSq.superior = [5.8, 4.71, 4.48, 5.68, 7.91, 8.08, 8.57, 9.38, 9.23, 8.53, 10.44, 11.48, 13.32, 11.9, 9.93];
-parasol.proportion.supportMM.inferior = [.96, 1.99, 2.98, 3.93, 4.93, 5.99, 6.87, 7.94, 8.89, 9.88, 10.92, 11.91, 12.9, 13.89, 14.92];
-parasol.proportion.countsMMSq.inferior = [5.73, 5.58, 7.18, 5.6, 6.96, 8.72, 8.81, 10.01, 8.28, 9.4, 7.74, 9.42, 6.81, 4.68, nan];
+parasol.proportion.supportMM.nasal = [0 1.06, 2.07, 3.12, 4.12, 5.09, 6.17, 7.19, 8.22, 9.2, 10.24, 11.26, 12.24, 13.31, 14.32, 15.32, 16.42, 17.39];
+parasol.proportion.value.nasal = [0 9.8, 9.64, nan, nan, nan, 7.58, 9.5, 11.74, 13.98, 19.18, 20.78, 15.99, 19.83, 20.32, 24.55, 26.87, 19.93];
+parasol.proportion.supportMM.temporal = [0 1.06, 2.07, 3.12, 4.12, 5.09, 6.17, 7.19, 8.22, 9.2, 10.24, 11.26, 12.24, 13.31, 14.32, 15.32, 16.42, 17.39];
+parasol.proportion.value.temporal = [0 6.6, 8.44, 9.09, 8.45, 8.85, 10.54, 10.86, 9.51, 9.27, 8.56, 5.69, 2.97, 4.98, nan, nan, nan, nan];
+parasol.proportion.supportMM.superior = [0 .96, 1.99, 2.98, 3.93, 4.93, 5.99, 6.87, 7.94, 8.89, 9.88, 10.92, 11.91, 12.9, 13.89, 14.92];
+parasol.proportion.value.superior = [0 5.8, 4.71, 4.48, 5.68, 7.91, 8.08, 8.57, 9.38, 9.23, 8.53, 10.44, 11.48, 13.32, 11.9, 9.93];
+parasol.proportion.supportMM.inferior = [0 .96, 1.99, 2.98, 3.93, 4.93, 5.99, 6.87, 7.94, 8.89, 9.88, 10.92, 11.91, 12.9, 13.89, 14.92];
+parasol.proportion.value.inferior = [0 5.73, 5.58, 7.18, 5.6, 6.96, 8.72, 8.81, 10.01, 8.28, 9.4, 7.74, 9.42, 6.81, 4.68, nan];
+
+% Obtain a spline fit to the parasol densities
+for mm = 1:length(p.Results.cardinalMeridianAngles)
+    nonNanSupportIdx = ~isnan(parasol.proportion.value.(p.Results.cardinalMeridianNames{mm}));
+    tmpSupport = parasol.proportion.supportMM.(p.Results.cardinalMeridianNames{mm})(nonNanSupportIdx)';
+    parasol.density.fitMMSq.(p.Results.cardinalMeridianNames{mm}) = ...
+        fit( tmpSupport, ...        
+        totalRGC.density.fitMMSq.(p.Results.cardinalMeridianNames{mm})(tmpSupport)' .* ...
+        parasol.proportion.value.(p.Results.cardinalMeridianNames{mm})(nonNanSupportIdx)' ./100, ...
+        'smoothingspline');
+end
 
 % Parasol cell body sizes
 % Data from Perry et al. 1984, Figure 6C
 
+%% KARA -- The size values below look like microns instead of mm
+
 % Perry, V. H., R. Oehler, and A. Cowey. "Retinal ganglion cells that project to the dorsal
 % lateral geniculate nucleus in the macaque monkey." Neuroscience 12.4 (1984): 1101-1123.
+% ARE WE CERTAIN THE SUPPRT IS IN MM?
 parasol.diameter.supportMM = [0.53, 1.07, 1.47, 1.96, 2.5, 3.1, 3.5, 4, 4.55, 5, 5.53, 6, 6.47, 7, 7.53, 8.05, 9, 9.65, 10.33, 11.4, 12.66, 13.63, 14.21];
 parasol.diameter.sizeMM = [11.27, 12.57, 13.79, 15.02, 16.16, 14.46, 18.23, 18.69, 19.98, 20.9, 22.05, 22.51, 23.35, 23.88, 24.34, 23.5, 27.4, 25.95, 24.19, 23.81, 22.74, 22.35, 20.11];
+parasol.diameter.fitMM = fit(parasol.diameter.supportMM', parasol.diameter.sizeMM','smoothingspline');
 
 
 %% Bistratified RGCs
 
-% Bistratified cell density
-bistratified.density.supportMM.temporal = [];
-bistratified.density.countsMMSq.temporal = [];
-
-% Kara -- you may just have proportion values instead of density for the bistratified. Put the numbers here
+% Kara -- you may just have proportion values instead of density for the
+% bistratified. Put the numbers here
 bistratified.proportion.supportMM.temporal = [];
 bistratified.proportion.countsMMSq.temporal = [];
 
-
 % Bistratified cell body sizes
-% Data from Peterson & Dacey 2000, Figure 3B
-
-% Peterson, Beth B., and Dennis M. Dacey. "Morphology of wide-field bistratified and 
-% diffuse human retinal ganglion cells." Visual neuroscience 17.4 (2000): 567-578.
+% Data from Figure 3B:
+%   Peterson, Beth B., and Dennis M. Dacey. "Morphology of wide-field
+%   bistratified and diffuse human retinal ganglion cells." Visual
+%   neuroscience 17.4 (2000): 567-578.
 
 bistratified.diameter.supportMM = [nan];
 bistratified.diameter.sizeMM = [.018];
-
+bistratified.diameter.fitMM = @(x) 0.018;
 
 %% Midget RGCs
 
@@ -142,24 +149,42 @@ end
 
 
 % Midget cell body sizes
-% Data from Liu 2017, Figure 4B
-% The support is in range of degrees 
-% Liu, Zhuolin, et al. "Imaging and quantifying ganglion cells and other transparent neurons
-% in the living human retina." Proceedings of the National Academy of Sciences (2017): 201711734.
-midget.diameter.supportMM = [1.5-3, 3-4.5, 6-7.5, 8-9.5, 12-13.5];
+% Data from Figure 4B:
+%   Liu, Zhuolin, et al. "Imaging and quantifying ganglion cells and other
+%   transparent neurons in the living human retina." Proceedings of the
+%   National Academy of Sciences (2017): 201711734.
+
+% Support in the source data is in degrees (of visual field or retina??) 
+midget.diameter.supportDeg = [(1.5+3)/2, (3+4.5)/2, (6+7.5)/2, (8+9.5)/2, (12+13.5)/2];
+% Are the data from the horizontal meridian? Temporal or nasal retina?
+midget.diameter.supportMM = convert_degVisual_to_mmRetina(midget.diameter.supportDeg, 0);
 midget.diameter.sizeMM = [.0115, .0113, .0114, .0118, .01315];
 
+% Obtain a spline fit
+midget.diameter.fitMM = fit(midget.diameter.supportMM', midget.diameter.sizeMM', 'smoothingspline');
 
-% Calculate thickness
-
+% Figure prep
+figure
 supportMM = 0:0.01:6;
-plot(totalRGC.density.fitMMSq.temporal(supportMM) + amacrine.density.fitMMSq.temporal(supportMM))
-hold on
-plot(midget.density.fitMMSq.temporal(supportMM))
-plot(parasol.density.fitMMSq.temporal(supportMM).*100)
-plot(amacrine.density.fitMMSq.temporal(supportMM))
 
-plot(midget.density.fitMMSq.temporal(supportMM) + parasol.density.fitMMSq.temporal(supportMM).*100 + amacrine.density.fitMMSq.temporal(supportMM),'xr')
+% Plot counts
+subplot(1,2,1)
+plot(supportMM, totalRGC.density.fitMMSq.temporal(supportMM) + amacrine.density.fitMMSq.temporal(supportMM))
+hold on
+plot(supportMM, midget.density.fitMMSq.temporal(supportMM))
+plot(supportMM, parasol.density.fitMMSq.temporal(supportMM))
+plot(supportMM, amacrine.density.fitMMSq.temporal(supportMM))
+plot(supportMM, midget.density.fitMMSq.temporal(supportMM) + parasol.density.fitMMSq.temporal(supportMM) + amacrine.density.fitMMSq.temporal(supportMM),'xr')
+
+sVol = @(d) 4/3*pi*(d./2).^3;
+
+% Plot volume
+subplot(1,2,2)
+volumeProfile = amacrine.density.fitMMSq.temporal(supportMM) .* sVol(amacrine.diameter.fitMM(supportMM)) + ...
+    parasol.density.fitMMSq.temporal(supportMM) .* sVol(parasol.diameter.fitMM(supportMM)./1000) + ...
+    midget.density.fitMMSq.temporal(supportMM) .* sVol(midget.diameter.fitMM(supportMM));
+
+plot(supportMM, volumeProfile);
 
 end % rgcThickness function
 
