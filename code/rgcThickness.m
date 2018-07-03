74907c4f17106da4b747594e30c22a9cc59adc8e
kncloud <40438510+kncloud@users.noreply.github.com>
2018-07-03 14:21:54 -0400

    Added Curcio 2011 RGC and IPL thickness

--- code/rgcThickness.m
+++ code/rgcThickness.m
@@ -271,15 +271,21 @@ ylabel('layer thickness [mm]]');
 plot(supportMM, volumeProfile);
 
 
-%% Curcio RGC and IPL thickness measurements
-empiricalThickness.RGC.supportMM.temporal = [];
-empiricalThickness.RGC.thickMM.temporal = [];
-empiricalThickness.RGC.supportMM.nasal = [];
-empiricalThickness.RGC.thickMM.nasal = [];
-empiricalThickness.IPL.supportMM.temporal = [];
-empiricalThickness.IPL.thickMM.temporal = [];
-empiricalThickness.IPL.supportMM.nasal = [];
-empiricalThickness.IPL.thickMM.nasal = [];
+%% RGC and IPL thickness measurements as reported by Curcio 2011
+% determined by 18 histologically sectioned maculas.
+%   Curcio, Christine A., et al. "Human chorioretinal layer thicknesses 
+%   measured in macula-wide, high-resolution histologic sections." Investigative 
+%   ophthalmology & visual science 52.7 (2011): 3943-3954.
+%
+% Data from Curcio 2011, Figure 7B and 7C
+empiricalThickness.RGC.supportMM.temporal = [0, 0.09, 0.18, 0.26, 0.35, 0.4, 0.56, 0.69, 0.82, 0.91, 1.11, 1.26, 1.45, 1.7, 1.88, 2.06, 2.27, 2.48, 2.7, 2.94, 2.99];
+empiricalThickness.RGC.thickMM.temporal = [.00455, .00455, .01212, .02273, .03485, .04848, .05303, .05758, .06061, .05909, .05606, .05303, .05, .04545, .03636, .03485, .0303, .02727, .02121, .02121];
+empiricalThickness.RGC.supportMM.nasal = [3, 2.88, 2.72, 2.54, 2.35, 2.15, 1.99, 1.86, 1.68, 1.53, 1.33, 1.14, 1.03, 0.87, 0.7, 0.6, 0.48, 0.31, 0.19, 0.11, 0.05, 0];
+empiricalThickness.RGC.thickMM.nasal = [.02121, .02121, .02273, .02576, .0303, .03485, .03788, .04394, .05, .05455, .07212, .06818, .07273, .07273, .07121, .0697, .05909, .05303, .03939, .0197, .00909, .00455];
+empiricalThickness.IPL.supportMM.temporal = [0, 0.1, 0.23, 0.38, 0.59, 0.79, 1.04, 1.31, 1.48, 1.76, 1.98, 2.19, 2.45, 2.75, 3];
+empiricalThickness.IPL.thickMM.temporal = [.00303, .00758, .01667, .02273, .02879, .03333, .03636, .03939, .03939, .04091, .04091, .03939, .03788, .03636, .03333];
+empiricalThickness.IPL.supportMM.nasal = [3, 2.86, 2.69, 2.55, 2.34, 2.13, 1.92, 1.69, 1.56, 1.34, 1.15, 0.84, 0.55, 0.37, 0.13, 0.06, 0];
+empiricalThickness.IPL.thickMM.nasal = [.0303, .03182, .03182, .03182, .03333, .03485, .03636, .03788, .03939, .03939, .03788, .03485, .02879, .02121, .00909, .00455, .00303];
 
 
 end % rgcThickness function
